6bbcaaaeb4d45a397337397c44824b607769016e
SiPlus <hwguy.siplus@gmail.com>
2013-09-05 20:59:26 +0400

    Android.mk

---
+++ src_main/DOGBuild.mk
@@ -0,0 +1,15 @@
+DOG_SRC_MAIN := $(call my-dir)
+LOCAL_PATH := $(DOG_SRC_MAIN)/$(DOG_PROJECT)
+
+include $(DOG_PROJECT)/DOGBuild.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := $(DOG_PROJECT)
+include DOGBuildCore.mk
+
+ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+	include $(CLEAR_VARS)
+	LOCAL_ARM_NEON := true
+	LOCAL_MODULE := $(DOG_PROJECT)_neon
+include DOGBuildCore.mk
+endif
---
+++ src_main/cstd/DOGBuild.mk
@@ -0,0 +1,3 @@
+DOG_SRC_FILES := \
+	wcsicmp.cpp
+DOG_STATIC_LIBRARY := true
