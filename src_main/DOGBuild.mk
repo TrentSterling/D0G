89e83f4febde44db2c08f74a16e032c0694c86cc
SiPlus <hwguy.siplus@gmail.com>
2013-09-19 21:14:45 +0400

    NativeActivity build rules
    The NativeActivity must be built without the GNU C++ STL shared library,
    separate NEON version and separate architecture names.

--- src_main/DOGBuild.mk
+++ src_main/DOGBuild.mk
@@ -3,16 +3,18 @@ LOCAL_PATH := $(DOG_SRC_MAIN)/$(DOG_PROJECT)
 
 include $(DOG_PROJECT)/DOGBuild.mk
 
-ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 
   include $(CLEAR_VARS)
   DOG_LIBSUFFIX := arm
   include DOGBuildCore.mk
 
-  include $(CLEAR_VARS)
-  DOG_LIBSUFFIX := neon
-  LOCAL_ARM_NEON := true
-  include DOGBuildCore.mk
+  ifneq ($(DOG_PROJECT),srcactivity)
+    include $(CLEAR_VARS)
+    DOG_LIBSUFFIX := neon
+    LOCAL_ARM_NEON := true
+    include DOGBuildCore.mk
+  endif
 
 else
 
--- src_main/DOGBuildCore.mk
+++ src_main/DOGBuildCore.mk
@@ -4,19 +4,27 @@ LOCAL_C_INCLUDES := \
   "$(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/$(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)/include" \
   "$(DOG_SRC_MAIN)/common" "$(DOG_SRC_MAIN)/public" $(DOG_C_INCLUDES)
 LOCAL_CFLAGS := -D_D0G_PACKAGE_NAME=\"$(DOG_PACKAGE_NAME)\" -D_LINUX=(1) -DWCHAR_MAX=(65535) -DWCHAR_MIN=(0) \
-  -fexceptions -ffast-math -fpermissive -frtti -fshort-wchar -O2 \
-  -Wno-attributes -Wno-invalid-offsetof -Wno-write-strings $(DOG_CFLAGS)
-LOCAL_MODULE := $(DOG_PROJECT)_android_$(DOG_LIBSUFFIX)
+  -ffast-math -fpermissive -fshort-wchar
+ifneq ($(DOG_PROJECT),srcactivity)
+  LOCAL_CFLAGS += -fexceptions -frtti
+endif
+LOCAL_CFLAGS += -O2 -Wno-attributes -Wno-invalid-offsetof -Wno-write-strings $(DOG_CFLAGS)
+ifeq ($(DOG_PROJECT),srcactivity)
+  LOCAL_MODULE := srcactivity
+else
+  LOCAL_MODULE := $(DOG_PROJECT)_android_$(DOG_LIBSUFFIX)
+endif
 LOCAL_SRC_FILES := $(DOG_SRC_FILES)
 ifeq ($(DOG_STATIC_LIBRARY),true)
   LOCAL_CFLAGS += -D_LIB -D_STATIC_LINKED
   include $(BUILD_STATIC_LIBRARY)
 else
   LOCAL_CFLAGS += -D_SHARED_LIB -D_USRDLL
+  LOCAL_LDFLAGS := -shared
+  ifneq ($(DOG_PROJECT),srcactivity)
+    LOCAL_LDFLAGS += -L"$(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/$(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)" -lgnustl_shared -lsupc++
+  endif
   DOG_LIBRARIES_PREFIXED := $(addprefix -l,$(DOG_SYSTEM_LIBRARIES) $(addsuffix _android_$(DOG_LIBSUFFIX),$(DOG_PUBLIC_LIBRARIES)))
-  LOCAL_LDFLAGS := -shared \
-    -L"$(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/$(TOOLCHAIN_VERSION)/libs/$(TARGET_ARCH_ABI)" \
-    -L"$(DOG_SRC_MAIN)/bin" -L"$(DOG_SRC_MAIN)/lib" \
-    -lgnustl_shared -Lsupc++ $(DOG_LIBRARIES_PREFIXED) $(DOG_LIBRARIES_PREFIXED)
+  LOCAL_LDFLAGS += -L"$(DOG_SRC_MAIN)/bin" -L"$(DOG_SRC_MAIN)/lib" $(DOG_LIBRARIES_PREFIXED) $(DOG_LIBRARIES_PREFIXED)
   include $(BUILD_SHARED_LIBRARY)
 endif
