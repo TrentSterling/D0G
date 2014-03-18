ebdfa7f7db3f503a803bc9371949223cb4f9c848
SiPlus <hwguy.siplus@gmail.com>
2014-03-18 13:24:09 +0400

    stdshader_oes2 makefile indev

---
+++ src_main/DOGBuild/stdshader_oes2.mk
@@ -0,0 +1,23 @@
+DOG_C_DEFINES := \
+  DEFINE_MATERIALSYSTEM_INTERFACE \
+  MATERIALSYSTEM_EXPORTS
+DOG_C_INCLUDES := \
+  "$(DOG_SRC_MAIN)/public/tier0" \
+  "$(DOG_SRC_MAIN)/public/tier1"
+DOG_PUBLIC_LIBRARIES := \
+  mathlib \
+  shaderlib \
+  tier0 \
+  tier1 \
+  vstdlib
+DOG_SRC_FILES := \
+  public/tier0/memoverride.cpp \
+  materialsystem/stdshader_oes2/accumbuff4sample.cpp \
+  materialsystem/stdshader_oes2/accumbuff5sample.cpp \
+  materialsystem/stdshader_oes2/BaseOES2Shader.cpp \
+  materialsystem/stdshader_oes2/bufferclearobeystencil.cpp \
+  materialsystem/stdshader_oes2/cable.cpp \
+  materialsystem/stdshader_oes2/cloud.cpp \
+  materialsystem/stdshader_oes2/downsample.cpp \
+  materialsystem/stdshader_oes2/unlitgeneric.cpp \
+  materialsystem/stdshader_oes2/vertexlitgeneric.cpp
