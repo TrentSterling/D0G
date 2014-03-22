9009bb233e352d70c38c575fb4ac043747254c01
SiPlus <hwguy.siplus@gmail.com>
2014-03-22 14:20:12 +0400

    FreeType mk

--- src_main/DOGBuild/freetype.mk
+++ src_main/DOGBuild/freetype.mk
@@ -1,163 +1,44 @@
 DOG_C_DEFINES := FT2_BUILD_LIBRARY
 DOG_C_INCLUDES := "$(DOG_SRC_MAIN)/common/freetype"
 DOG_SRC_FILES := \
-  vgui2/vgui_surfacelib/freetype/autofit/afangles.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afblue.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afcjk.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afdummy.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afglobal.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afhints.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afindic.c \
-  vgui2/vgui_surfacelib/freetype/autofit/aflatin.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afloader.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afmodule.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afpic.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afranges.c \
-  vgui2/vgui_surfacelib/freetype/autofit/afwarp.c \
   vgui2/vgui_surfacelib/freetype/autofit/autofit.c \
-  vgui2/vgui_surfacelib/freetype/autofit/hbshim.c \
-  vgui2/vgui_surfacelib/freetype/base/basepic.c \
-  vgui2/vgui_surfacelib/freetype/base/ftadvanc.c \
-  vgui2/vgui_surfacelib/freetype/base/ftapi.c \
   vgui2/vgui_surfacelib/freetype/base/ftbase.c \
   vgui2/vgui_surfacelib/freetype/base/ftbbox.c \
   vgui2/vgui_surfacelib/freetype/base/ftbdf.c \
   vgui2/vgui_surfacelib/freetype/base/ftbitmap.c \
-  vgui2/vgui_surfacelib/freetype/base/ftcalc.c \
-  vgui2/vgui_surfacelib/freetype/base/ftcid.c \
-  vgui2/vgui_surfacelib/freetype/base/ftdbgmem.c \
   vgui2/vgui_surfacelib/freetype/base/ftdebug.c \
-  vgui2/vgui_surfacelib/freetype/base/ftfstype.c \
   vgui2/vgui_surfacelib/freetype/base/ftgasp.c \
-  vgui2/vgui_surfacelib/freetype/base/ftgloadr.c \
   vgui2/vgui_surfacelib/freetype/base/ftglyph.c \
   vgui2/vgui_surfacelib/freetype/base/ftgxval.c \
   vgui2/vgui_surfacelib/freetype/base/ftinit.c \
   vgui2/vgui_surfacelib/freetype/base/ftlcdfil.c \
   vgui2/vgui_surfacelib/freetype/base/ftmm.c \
-  vgui2/vgui_surfacelib/freetype/base/ftobjs.c \
   vgui2/vgui_surfacelib/freetype/base/ftotval.c \
-  vgui2/vgui_surfacelib/freetype/base/ftoutln.c \
   vgui2/vgui_surfacelib/freetype/base/ftpatent.c \
   vgui2/vgui_surfacelib/freetype/base/ftpfr.c \
-  vgui2/vgui_surfacelib/freetype/base/ftpic.c \
-  vgui2/vgui_surfacelib/freetype/base/ftrfork.c \
-  vgui2/vgui_surfacelib/freetype/base/ftsnames.c \
-  vgui2/vgui_surfacelib/freetype/base/ftstream.c \
   vgui2/vgui_surfacelib/freetype/base/ftstroke.c \
   vgui2/vgui_surfacelib/freetype/base/ftsynth.c \
   vgui2/vgui_surfacelib/freetype/base/ftsystem.c \
-  vgui2/vgui_surfacelib/freetype/base/fttrigon.c \
   vgui2/vgui_surfacelib/freetype/base/fttype1.c \
-  vgui2/vgui_surfacelib/freetype/base/ftutil.c \
-  vgui2/vgui_surfacelib/freetype/base/ftwinfnt.c \
   vgui2/vgui_surfacelib/freetype/base/ftxf86.c \
-  vgui2/vgui_surfacelib/freetype/base/md5.c \
+  vgui2/vgui_surfacelib/freetype/base/ftwinfnt.c \
   vgui2/vgui_surfacelib/freetype/bdf/bdf.c \
-  vgui2/vgui_surfacelib/freetype/bdf/bdfdrivr.c \
-  vgui2/vgui_surfacelib/freetype/bdf/bdflib.c \
   vgui2/vgui_surfacelib/freetype/bzip2/ftbzip2.c \
   vgui2/vgui_surfacelib/freetype/cache/ftcache.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftcbasic.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftccache.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftccmap.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftcglyph.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftcimage.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftcmanag.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftcmru.c \
-  vgui2/vgui_surfacelib/freetype/cache/ftcsbits.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2arrst.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2blues.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2error.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2font.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2ft.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2hints.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2intrp.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2read.c \
-  vgui2/vgui_surfacelib/freetype/cff/cf2stack.c \
   vgui2/vgui_surfacelib/freetype/cff/cff.c \
-  vgui2/vgui_surfacelib/freetype/cff/cffcmap.c \
-  vgui2/vgui_surfacelib/freetype/cff/cffdrivr.c \
-  vgui2/vgui_surfacelib/freetype/cff/cffgload.c \
-  vgui2/vgui_surfacelib/freetype/cff/cffload.c \
-  vgui2/vgui_surfacelib/freetype/cff/cffobjs.c \
-  vgui2/vgui_surfacelib/freetype/cff/cffparse.c \
-  vgui2/vgui_surfacelib/freetype/cff/cffpic.c \
-  vgui2/vgui_surfacelib/freetype/cid/cidgload.c \
-  vgui2/vgui_surfacelib/freetype/cid/cidload.c \
-  vgui2/vgui_surfacelib/freetype/cid/cidobjs.c \
-  vgui2/vgui_surfacelib/freetype/cid/cidparse.c \
-  vgui2/vgui_surfacelib/freetype/cid/cidriver.c \
   vgui2/vgui_surfacelib/freetype/cid/type1cid.c \
   vgui2/vgui_surfacelib/freetype/gzip/ftgzip.c \
   vgui2/vgui_surfacelib/freetype/lzw/ftlzw.c \
-  vgui2/vgui_surfacelib/freetype/lzw/ftzopen.c \
   vgui2/vgui_surfacelib/freetype/pcf/pcf.c \
-  vgui2/vgui_surfacelib/freetype/pcf/pcfdrivr.c \
-  vgui2/vgui_surfacelib/freetype/pcf/pcfread.c \
-  vgui2/vgui_surfacelib/freetype/pcf/pcfutil.c \
   vgui2/vgui_surfacelib/freetype/pfr/pfr.c \
-  vgui2/vgui_surfacelib/freetype/pfr/pfrcmap.c \
-  vgui2/vgui_surfacelib/freetype/pfr/pfrdrivr.c \
-  vgui2/vgui_surfacelib/freetype/pfr/pfrgload.c \
-  vgui2/vgui_surfacelib/freetype/pfr/pfrload.c \
-  vgui2/vgui_surfacelib/freetype/pfr/pfrobjs.c \
-  vgui2/vgui_surfacelib/freetype/pfr/pfrsbit.c \
-  vgui2/vgui_surfacelib/freetype/psaux/afmparse.c \
   vgui2/vgui_surfacelib/freetype/psaux/psaux.c \
-  vgui2/vgui_surfacelib/freetype/psaux/psauxmod.c \
-  vgui2/vgui_surfacelib/freetype/psaux/psconv.c \
-  vgui2/vgui_surfacelib/freetype/psaux/psobjs.c \
-  vgui2/vgui_surfacelib/freetype/psaux/t1cmap.c \
-  vgui2/vgui_surfacelib/freetype/psaux/t1decode.c \
-  vgui2/vgui_surfacelib/freetype/pshinter/pshalgo.c \
-  vgui2/vgui_surfacelib/freetype/pshinter/pshglob.c \
   vgui2/vgui_surfacelib/freetype/pshinter/pshinter.c \
-  vgui2/vgui_surfacelib/freetype/pshinter/pshmod.c \
-  vgui2/vgui_surfacelib/freetype/pshinter/pshpic.c \
-  vgui2/vgui_surfacelib/freetype/pshinter/pshrec.c \
-  vgui2/vgui_surfacelib/freetype/psnames/psmodule.c \
   vgui2/vgui_surfacelib/freetype/psnames/psnames.c \
-  vgui2/vgui_surfacelib/freetype/psnames/pspic.c \
-  vgui2/vgui_surfacelib/freetype/raster/ftraster.c \
-  vgui2/vgui_surfacelib/freetype/raster/ftrend1.c \
   vgui2/vgui_surfacelib/freetype/raster/raster.c \
-  vgui2/vgui_surfacelib/freetype/raster/rastpic.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/pngshim.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/sfdriver.c \
   vgui2/vgui_surfacelib/freetype/sfnt/sfnt.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/sfntpic.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/sfobjs.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/ttbdf.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/ttcmap.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/ttkern.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/ttload.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/ttmtx.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/ttpost.c \
-  vgui2/vgui_surfacelib/freetype/sfnt/ttsbit.c \
-  vgui2/vgui_surfacelib/freetype/smooth/ftgrays.c \
-  vgui2/vgui_surfacelib/freetype/smooth/ftsmooth.c \
-  vgui2/vgui_surfacelib/freetype/smooth/ftspic.c \
   vgui2/vgui_surfacelib/freetype/smooth/smooth.c \
   vgui2/vgui_surfacelib/freetype/truetype/truetype.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttdriver.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttgload.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttgxvar.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttinterp.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttobjs.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttpic.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttpload.c \
-  vgui2/vgui_surfacelib/freetype/truetype/ttsubpix.c \
-  vgui2/vgui_surfacelib/freetype/type1/t1afm.c \
-  vgui2/vgui_surfacelib/freetype/type1/t1driver.c \
-  vgui2/vgui_surfacelib/freetype/type1/t1gload.c \
-  vgui2/vgui_surfacelib/freetype/type1/t1load.c \
-  vgui2/vgui_surfacelib/freetype/type1/t1objs.c \
-  vgui2/vgui_surfacelib/freetype/type1/t1parse.c \
   vgui2/vgui_surfacelib/freetype/type1/type1.c \
-  vgui2/vgui_surfacelib/freetype/type42/t42drivr.c \
-  vgui2/vgui_surfacelib/freetype/type42/t42objs.c \
-  vgui2/vgui_surfacelib/freetype/type42/t42parse.c \
   vgui2/vgui_surfacelib/freetype/type42/type42.c \
   vgui2/vgui_surfacelib/freetype/winfonts/winfnt.c
 DOG_STATIC_LIBRARY := true
